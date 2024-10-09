f6988bcc0f334e1ede6981a139093590b621c515
KN9N3k9 <153736172+Keanansolo143@users.noreply.github.com>
2024-10-09 12:52:14 +0200

    Module-10-Assessment-2

---
+++ index.js
@@ -0,0 +1,82 @@
+//list-recipes script
+
+let category = getCategory();
+
+function getRecipeList() {
+    let rootPath = "https://mysite.itvarsity.org/api/recipe-book/";
+    let fullPath = rootPath + "get-recipes/?category=" + category;
+
+    fetch(fullPath)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            let output = "";
+
+            for (a = 0; a < data.length; a++) {
+                output += `
+                    <a href="show-recipe.html?id=${data[a].id}">
+                        <div class="meals-list-item">
+                            <h1> ${data[a].title}<i class="fa-solid fa-circle-chevron-right"></i></h1>
+                            <p>${data[a].description}</p>
+                        </div>
+                    </a>
+                `;
+            };
+
+            document.getElementById("output").innerHTML = output;
+        })
+}
+
+function getCategory() {
+    let url = window.location.href;
+    let pos = url.search("=");
+    let category = url.slice(pos + 1);
+
+    return category;
+}
+
+//show-recipes script
+
+let id = getId();
+console.log(id);
+
+function getRecipe() {
+    let rootPath = "https://mysite.itvarsity.org/api/recipe-book/";
+    let fullPath = rootPath + "get-recipes/?id=" + id;
+
+    fetch(fullPath)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            document.getElementById("back-link").href = "list-recipes.html?category=" + data[0].category;
+            let output = "";
+
+            output += `
+                <img src="${rootPath}uploads/${data[0].image}"/>
+                <div class="recipes-details-ingredients">
+                    <h2>Ingredients</h2>
+                    <ul>
+                        ${data[0].ingredients}
+                    </ul>
+                </div>
+                <div class="recipes-details-steps">
+                    <h2>Method</h2>
+                    <ol>
+                        ${data[0].method}
+                    </ol>
+                </div>
+            `;
+            document.getElementById("output").innerHTML = output;
+            console.log(data);
+        })
+}
+
+function getId() {
+    let url = window.location.href;
+    let pos = url.search("=");
+    let id = url.slice(pos + 1);
+
+    return id;
+}
